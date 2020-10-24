15aa94ff861ab9ce94b98ddc3e17a8985257c652
99002757 <72466426+99002757@users.noreply.github.com>
2020-10-24 19:07:21 +0530

    Delete Two.js

--- User/Two.js
+++
@@ -1,9 +0,0 @@
-const app = require("express")();
-
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/Client.html");
-})
-
-app.listen(3333, () => {
-    console.log("Client App running at 3333");
-})
